fatih/vim-go

commit accc8959140fcb896bb76249bdd87a20052f6411

Adds a do not prefill :GoRename setting
Added a setting to prevent :GoRename from prefilling. Also if the to
identifer is empty, just return. This way if you run :GoRename but
decide to not rename, simply press escape.

autoload/go/rename.vim
@@ -2,21 +2,32 @@ if !exists("g:go_gorename_bin")
     let g:go_gorename_bin = "gorename"
 endif
 
+if !exists("g:go_gorename_prefill")
+    let g:go_gorename_prefill = 1
+endif
+
 function! go#rename#Rename(bang, ...)
     let to = ""
     if a:0 == 0
         let from = expand("<cword>")
         let ask = printf("vim-go: rename '%s' to: ", from)
-        let to = input(ask, from)
-        redraw
+        if g:go_gorename_prefill
+            let to = input(ask, from)
+        else
+            let to = input(ask)
+        endif
+        redraw!
+        if empty(to)
+            return
+        endif
     else
         let to = a:1
     endif
 
     "return with a warning if the bin doesn't exist
-    let bin_path = go#path#CheckBinPath(g:go_gorename_bin) 
-    if empty(bin_path) 
-        return 
+    let bin_path = go#path#CheckBinPath(g:go_gorename_bin)
+    if empty(bin_path)
+        return
     endif
 
     let fname = expand('%:p')

commit 45238d71c1555bee79c5a01049467c85b2d613fd

Fix bad syntax of `redraw` in go/rename.vim
This issue is a regression caused by commit: accc895

autoload/go/rename.vim
@@ -16,7 +16,7 @@ function! go#rename#Rename(bang, ...)
         else
             let to = input(ask)
         endif
-        redraw!
+        redraw
         if empty(to)
             return
         endif