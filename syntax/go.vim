fatih/vim-go

commit cfa9713ebe5bf5297b7d0d7ee3cbb45968f9b5b9

Match zero-width in goTypeConstructor
The `{` here interferes with the folding in:

	syn region      goBlock             start="{" end="}" transparent fold

I'm not sure if this is the best way to fix it; but it seems to work well as far
as I can see. I also tried mucking about with `contained` and such, but wasn't
able to get that to work.

Fixes #1282

syntax/go.vim
@@ -321,7 +321,7 @@ hi def link    goField              Identifier
 
 " Structs & Interfaces;
 if g:go_highlight_types != 0
-  syn match goTypeConstructor      /\<\w\+{/he=e-1
+  syn match goTypeConstructor      /\<\w\+{\@=/he=e-1 
   syn match goTypeDecl             /\<type\>/ nextgroup=goTypeName skipwhite skipnl
   syn match goTypeName             /\w\+/ contained nextgroup=goDeclType skipwhite skipnl
   syn match goDeclType             /\<\(interface\|struct\)\>/ skipwhite skipnl

commit dd6e154689b1902805904d79125343a41b2cbab0

Don't add offset to goTypeConstructor.
This is a regression from cfa9713. Previously the offset was needed since the
`{` was matched as well. Now we don't match the `{` the offset means the last
character of the type won't be matched.

Fixes #1357

syntax/go.vim
@@ -321,7 +321,7 @@ hi def link    goField              Identifier
 
 " Structs & Interfaces;
 if g:go_highlight_types != 0
-  syn match goTypeConstructor      /\<\w\+{\@=/he=e-1 
+  syn match goTypeConstructor      /\<\w\+{\@=/
   syn match goTypeDecl             /\<type\>/ nextgroup=goTypeName skipwhite skipnl
   syn match goTypeName             /\w\+/ contained nextgroup=goDeclType skipwhite skipnl
   syn match goDeclType             /\<\(interface\|struct\)\>/ skipwhite skipnl