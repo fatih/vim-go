fatih/vim-go

commit 3b5b1480fce1bb41426ddee270d36c3ae740d71b

Add :GoDebugTest (#1728)
* cd in to directory before debugging test files

This is also what "go test" seems to do:

    [~/go/src/a]% cat *.go
    package a

    import (
            "fmt"
            "os"
    )

    func a() {
            d, _ := os.Getwd()
            fmt.Println("a.go:", d)
    }
    package a

    import (
            "fmt"
            "os"
            "testing"
    )

    func TestA(t *testing.T) {
            a()
            d, _ := os.Getwd()
            fmt.Println("a_test.go:", d)
    }

    [~/go/src/a]% go test ./... -v
    === RUN   TestA
    a.go: /home/martin/go/src/a
    a_test.go: /home/martin/go/src/a
    --- PASS: TestA (0.00s)
    PASS
    ok      a       0.001s
    === RUN   TestB
    b_test.go: /home/martin/go/src/a/b
    b_test.go: /home/martin/go/src/a/b
    --- PASS: TestB (0.00s)
    PASS
    ok      a/b     0.001s

Seems to make sense to match this behaviour.

Fixes part of #1690

* Add :GoDebugTest

Imagine I'm editing `a_test.go`, which is failing. To investigate I want
to add a breakpoint in a function in `a.go`. The current procedure for
that is something like:

- `:e a.go`
- `:GoDebugBreakpoint`
- `:e a_test.go`
- `:GoDebugStart`
- `<F5>`
- Buffer will switch back to `a.go`

The `:e_test.go` is needed because the only way to run `dlv test`
instead of `dlv debug` is to have the current buffer end in `_test.go`.

I think this is rather awkward, so add `:GoDebugTest` instead of trying
to guess what the users wants so you now can do:

- `:e a.go`
- `:GoDebugBreakpoint`
- `:GoDebugTest`
- `<F5>`

Fixes the remainder of #1690

* Set appropriate initial breakpoint

The debugger will set a breakpoint on `main.main` when starting a
program without any breakpoints set. This doesn't work in tests.

With this change it will try to get the function the cursor is in and
break on that. Come to think of it, this behaviour perhaps also makes

autoload/go/textobj.vim
@@ -17,6 +17,7 @@ endif
 " < >
 " t for tag
 
+" Select a function in visual mode.
 function! go#textobj#Function(mode) abort
   let offset = go#util#OffsetCursor()
 
@@ -98,23 +99,8 @@ function! go#textobj#Function(mode) abort
   call cursor(info.rbrace.line-1, 1)
 endfunction
 
-function! go#textobj#FunctionJump(mode, direction) abort
-  " get count of the motion. This should be done before all the normal
-  " expressions below as those reset this value(because they have zero
-  " count!). We abstract -1 because the index starts from 0 in motion.
-  let l:cnt = v:count1 - 1
-
-  " set context mark so we can jump back with  '' or ``
-  normal! m'
-
-  " select already previously selected visual content and continue from there.
-  " If it's the first time starts with the visual mode. This is needed so
-  " after selecting something in visual mode, every consecutive motion
-  " continues.
-  if a:mode == 'v'
-    normal! gv
-  endif
-
+" Get the location of the previous or next function.
+function! go#textobj#FunctionLocation(direction, cnt) abort
   let offset = go#util#OffsetCursor()
 
   let fname = shellescape(expand("%:p"))
@@ -131,7 +117,7 @@ function! go#textobj#FunctionJump(mode, direction) abort
   endif
 
   let command = printf("%s -format vim -file %s -offset %s", bin_path, fname, offset)
-  let command .= ' -shift ' . l:cnt
+  let command .= ' -shift ' . a:cnt
 
   if a:direction == 'next'
     let command .= ' -mode next'
@@ -149,14 +135,40 @@ function! go#textobj#FunctionJump(mode, direction) abort
     return
   endif
 
+  echom l:out
+
   " if exists, delete it as we don't need it anymore
   if exists("l:tmpname")
     call delete(l:tmpname)
   endif
 
-  " convert our string dict representation into native Vim dictionary type
-  let result = eval(out)
-  if type(result) != 4 || !has_key(result, 'fn')
+  let l:result = json_decode(out)
+  if type(l:result) != 4 || !has_key(l:result, 'fn')
+    return
+  endif
+
+  return l:result
+endfunction
+
+function! go#textobj#FunctionJump(mode, direction) abort
+  " get count of the motion. This should be done before all the normal
+  " expressions below as those reset this value(because they have zero
+  " count!). We abstract -1 because the index starts from 0 in motion.
+  let l:cnt = v:count1 - 1
+
+  " set context mark so we can jump back with  '' or ``
+  normal! m'
+
+  " select already previously selected visual content and continue from there.
+  " If it's the first time starts with the visual mode. This is needed so
+  " after selecting something in visual mode, every consecutive motion
+  " continues.
+  if a:mode == 'v'
+    normal! gv
+  endif
+
+  let l:result = go#textobj#FunctionLocation(a:direction, l:cnt)
+  if !l:result
     return
   endif
 

autoload/go/debug.vim
@@ -21,6 +21,7 @@ if !exists('s:state')
       \ 'localVars': {},
       \ 'functionArgs': {},
       \ 'message': [],
+      \ 'is_test': 0,
       \}
 
   if go#util#HasDebug('debugger-state')
@@ -250,7 +251,8 @@ function! go#debug#Stop() abort
   for k in map(split(execute('command GoDebug'), "\n")[1:], 'matchstr(v:val, "^\\s*\\zs\\S\\+")')
     exe 'delcommand' k
   endfor
-  command! -nargs=* -complete=customlist,go#package#Complete GoDebugStart call go#debug#Start(<f-args>)
+  command! -nargs=* -complete=customlist,go#package#Complete GoDebugStart call go#debug#Start(0, <f-args>)
+  command! -nargs=* -complete=customlist,go#package#Complete GoDebugTest  call go#debug#Start(1, <f-args>)
   command! -nargs=? GoDebugBreakpoint call go#debug#Breakpoint(<f-args>)
 
   " Remove all mappings.
@@ -442,6 +444,7 @@ function! s:start_cb(ch, json) abort
   endif
 
   silent! delcommand GoDebugStart
+  silent! delcommand GoDebugTest
   command! -nargs=0 GoDebugContinue   call go#debug#Stack('continue')
   command! -nargs=0 GoDebugNext       call go#debug#Stack('next')
   command! -nargs=0 GoDebugStep       call go#debug#Stack('step')
@@ -493,7 +496,7 @@ endfunction
 
 " Start the debug mode. The first argument is the package name to compile and
 " debug, anything else will be passed to the running program.
-function! go#debug#Start(...) abort
+function! go#debug#Start(is_test, ...) abort
   if has('nvim')
     call go#util#EchoError('This feature only works in Vim for now; Neovim is not (yet) supported. Sorry :-(')
     return
@@ -514,7 +517,12 @@ function! go#debug#Start(...) abort
     let g:go_debug_diag = s:state
   endif
 
-  let l:is_test = bufname('')[-8:] is# '_test.go'
+  " cd in to test directory; this is also what running "go test" does.
+  if a:is_test
+    lcd %:p:h
+  endif
+
+  let s:state.is_test = a:is_test
 
   let dlv = go#path#CheckBinPath("dlv")
   if empty(dlv)
@@ -539,7 +547,7 @@ function! go#debug#Start(...) abort
 
     let l:cmd = [
           \ dlv,
-          \ (l:is_test ? 'test' : 'debug'),
+          \ (a:is_test ? 'test' : 'debug'),
           \ '--output', tempname(),
           \ '--headless',
           \ '--api-version', '2',
@@ -766,14 +774,10 @@ function! go#debug#Stack(name) abort
 
   " Add a breakpoint to the main.Main if the user didn't define any.
   if len(s:state['breakpoint']) is 0
-    try
-      let res = s:call_jsonrpc('RPCServer.FindLocation', {'loc': 'main.main'})
-      let res = s:call_jsonrpc('RPCServer.CreateBreakpoint', {'Breakpoint': {'addr': res.result.Locations[0].pc}})
-      let bt = res.result.Breakpoint
-      let s:state['breakpoint'][bt.id] = bt
-    catch
-      call go#util#EchoError(v:exception)
-    endtry
+    if go#debug#Breakpoint() isnot 0
+      let s:state.running = 0
+      return
+    endif
   endif
 
   try
@@ -823,7 +827,7 @@ function! s:isActive()
   return len(s:state['message']) > 0
 endfunction
 
-" Toggle breakpoint.
+" Toggle breakpoint. Returns 0 on success and 1 on failure.
 function! go#debug#Breakpoint(...) abort
   let l:filename = fnamemodify(expand('%'), ':p:gs!\\!/!')
 
@@ -832,7 +836,7 @@ function! go#debug#Breakpoint(...) abort
     let linenr = str2nr(a:1)
     if linenr is 0
       call go#util#EchoError('not a number: ' . a:1)
-      return
+      return 0
     endif
   else
     let linenr = line('.')
@@ -871,7 +875,10 @@ function! go#debug#Breakpoint(...) abort
     endif
   catch
     call go#util#EchoError(v:exception)
+    return 1
   endtry
+
+  return 0
 endfunction
 
 sign define godebugbreakpoint text=> texthl=GoDebugBreakpoint

ftplugin/go/commands.vim
@@ -100,7 +100,8 @@ command! -nargs=0 GoFillStruct call go#fillstruct#FillStruct()
 
 " -- debug
 if !exists(':GoDebugStart')
-  command! -nargs=* -complete=customlist,go#package#Complete GoDebugStart call go#debug#Start(<f-args>)
+  command! -nargs=* -complete=customlist,go#package#Complete GoDebugStart call go#debug#Start(0, <f-args>)
+  command! -nargs=* -complete=customlist,go#package#Complete GoDebugTest  call go#debug#Start(1, <f-args>)
   command! -nargs=? GoDebugBreakpoint call go#debug#Breakpoint(<f-args>)
 endif
 

commit 73c88fd53cc15dbc57a0b23e9e50d551df12d357

Fix [[ and ]] jumping
Regression from #1728 when it was split in two functions.

autoload/go/textobj.vim
@@ -135,8 +135,6 @@ function! go#textobj#FunctionLocation(direction, cnt) abort
     return
   endif
 
-  echom l:out
-
   " if exists, delete it as we don't need it anymore
   if exists("l:tmpname")
     call delete(l:tmpname)
@@ -144,7 +142,7 @@ function! go#textobj#FunctionLocation(direction, cnt) abort
 
   let l:result = json_decode(out)
   if type(l:result) != 4 || !has_key(l:result, 'fn')
-    return
+    return 0
   endif
 
   return l:result
@@ -168,7 +166,7 @@ function! go#textobj#FunctionJump(mode, direction) abort
   endif
 
   let l:result = go#textobj#FunctionLocation(a:direction, l:cnt)
-  if !l:result
+  if l:result is 0
     return
   endif
 