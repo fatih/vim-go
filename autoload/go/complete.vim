fatih/vim-go

commit dac81d653dfcec98624a016dc3ba854959d7df9b

use gopls for autocompletion
Use gopls for autocompletion when jobs are available (e.g. Vim8 and
Neovim).

Cancel completion in when there are no matches available.

autoload/go/complete.vim
@@ -219,12 +219,22 @@ function! s:trim_bracket(val) abort
   return a:val
 endfunction
 
-let s:completions = ""
-function! go#complete#Complete(findstart, base) abort
+let s:completions = []
+
+function! go#complete#GocodeComplete(findstart, base) abort
   "findstart = 1 when we need to get the text length
   if a:findstart == 1
-    execute "silent let s:completions = " . s:gocodeAutocomplete()
-    return col('.') - s:completions[0] - 1
+    let l:completions = []
+    execute "silent let l:completions = " . s:gocodeAutocomplete()
+
+    if len(l:completions) == 0 || len(l:completions) >= 2 && len(l:completions[1]) == 0
+      " no matches. cancel and leave completion mode.
+      call go#util#EchoInfo("no matches")
+      return -3
+    endif
+
+    let s:completions = l:completions[1]
+    return col('.') - l:completions[0] - 1
     "findstart = 0 when we need to return the list of completions
   else
     let s = getline(".")[col('.') - 1]
@@ -233,6 +243,37 @@ function! go#complete#Complete(findstart, base) abort
     endif
 
     return s:completions[1]
+  endif
+endfunction
+
+function! go#complete#Complete(findstart, base) abort
+  let l:state = {'done': 0, 'matches': []}
+
+  function! s:handler(state, matches) abort dict
+    let a:state.matches = a:matches
+    let a:state.done = 1
+  endfunction
+
+  "findstart = 1 when we need to get the start of the match
+  if a:findstart == 1
+    call go#lsp#Completion(expand('%:p'), line('.'), col('.'), funcref('s:handler', [l:state]))
+
+    while !l:state.done
+      sleep 10m
+    endwhile
+
+    let s:completions = l:state.matches
+
+    if len(l:state.matches) == 0
+      " no matches. cancel and leave completion mode.
+      call go#util#EchoInfo("no matches")
+      return -3
+    endif
+
+    return col('.') - (len(l:state.matches[0].abbr) - len(l:state.matches[0].word))
+    "findstart = 0 when we need to return the list of completions
+  else
+    return s:completions
   endif
 endfunction
 

autoload/go/lsp.vim
@@ -33,7 +33,14 @@ function! s:newlsp()
   " queue is messages to send after initialization
   " last_request_id is id of the most recently sent request.
   " buf is unprocessed/incomplete responses
-  " handlers is request id -> {start, showstatus, handleResult}
+  " handlers is a mapping of request ids to dictionaries of functions.
+  "   request id -> {start, showstatus, handleResult, error}
+  "   * start is a function that takes no arguments
+  "   * showstatus is a function that takes 1 argument. The parameter will be 1
+  "     if the call was succesful.
+  "   * handleResult takes a single argument, the result message received from gopls
+  "   * error takes a single argument, the error message received from gopls.
+  "     The error method is optional.
   let l:lsp = {
         \ 'job':  '',
         \ 'ready': 0,
@@ -77,9 +84,7 @@ function! s:newlsp()
       endif
 
       if go#util#HasDebug('lsp')
-        let g:go_lsp_log = add(go#config#LspLog(), {
-              \ 'response': l:rest[:l:rest_start_idx - 1]
-        \ })
+        let g:go_lsp_log = add(go#config#LspLog(), "<-\n" . l:rest[:l:rest_start_idx - 1])
       endif
 
       let l:body = l:rest[l:body_start_idx : l:rest_start_idx - 1]
@@ -122,6 +127,9 @@ function! s:newlsp()
               if has_key(l:response, 'error')
                 call l:handler.showstatus(0)
                 call go#util#EchoError(l:response.error.message)
+                if has_key(l:handler, 'error')
+                  call call(l:handler.error, [l:response.error.message])
+                endif
                 return
               endif
               call l:handler.showstatus(1)
@@ -209,9 +217,7 @@ function! s:newlsp()
       let l:data = 'Content-Length: ' . strlen(l:body) . "\r\n\r\n" . l:body
 
     if go#util#HasDebug('lsp')
-      let g:go_lsp_log = add(go#config#LspLog(), {
-            \ 'request':  l:data,
-      \ })
+      let g:go_lsp_log = add(go#config#LspLog(), "->\n" . l:data)
     endif
 
     if has('nvim')
@@ -233,9 +239,7 @@ function! s:newlsp()
 
   function! l:lsp.err_cb(ch, msg) dict abort
     if go#util#HasDebug('lsp')
-      let g:go_lsp_log = add(go#config#LspLog(), {
-            \ 'stderr':  a:msg,
-      \ })
+      let g:go_lsp_log = add(go#config#LspLog(), "<-stderr\n" .  a:msg)
     endif
   endfunction
 
@@ -414,6 +418,32 @@ function! go#lsp#DidClose(fname)
   call l:lsp.sendMessage(l:msg, l:state)
 
   let b:go_lsp_did_open = 0
+endfunction
+
+function! go#lsp#Completion(fname, line, col, handler)
+  function! s:completionHandler(next, msg) abort dict
+    " gopls returns a CompletionList.
+    let l:matches = []
+    for l:item in a:msg.items
+      let l:match = {'abbr': l:item.label, 'word': l:item.textEdit.newText, 'info': l:item.detail, 'kind': go#lsp#completionitemkind#Vim(l:item.kind)}
+      if has_key(l:item, 'documentation')
+        let l:match.info .= "\n\n" . l:item.documentation
+      endif
+
+      let l:matches = add(l:matches, l:match)
+    endfor
+    let l:args = [l:matches]
+    call call(a:next, l:args)
+  endfunction
+
+  call go#lsp#DidChange(a:fname)
+
+  let l:lsp = s:lspfactory.get()
+  let l:msg = go#lsp#message#Completion(a:fname, a:line, a:col)
+  let l:state = s:newHandlerState('')
+  let l:state.handleResult = funcref('s:completionHandler', [function(a:handler, [], l:state)], l:state)
+  let l:state.error = funcref('s:completionHandler', [function(a:handler, [{'items': []}], l:state)], l:state)
+  call l:lsp.sendMessage(l:msg, l:state)
 endfunction
 
 " restore Vi compatibility settings

autoload/go/lsp/completionitemkind.vim
@@ -1,0 +1,47 @@
+" don't spam the user when Vim is started in Vi compatibility mode
+let s:cpo_save = &cpo
+set cpo&vim
+
+let s:Text = 1
+let s:Method = 2
+let s:Function = 3
+let s:Constructor = 4
+let s:Field = 5
+let s:Variable = 6
+let s:Class = 7
+let s:Interface = 8
+let s:Module = 9
+let s:Property = 10
+let s:Unit = 11
+let s:Value = 12
+let s:Enum = 13
+let s:Keyword = 14
+let s:Snippet = 15
+let s:Color = 16
+let s:File = 17
+let s:Reference = 18
+let s:Folder = 19
+let s:EnumMember = 20
+let s:Constant = 21
+let s:Struct = 22
+let s:Event = 23
+let s:Operator = 24
+let s:TypeParameter = 25
+
+function! go#lsp#completionitemkind#Vim(kind)
+  if a:kind == s:Method || a:kind == s:Function || a:kind == s:Constructor
+    return 'f'
+  elseif a:kind == s:Variable || a:kind == s:Constant
+    return 'v'
+  elseif a:kind == s:Field || a:kind == s:Property
+    return 'm'
+  elseif a:kind == s:Class || a:kind == s:Interface || a:kind == s:Struct
+    return 't'
+  endif
+endfunction
+
+" restore Vi compatibility settings
+let &cpo = s:cpo_save
+unlet s:cpo_save
+
+" vim: sw=2 ts=2 et

autoload/go/lsp/message.vim
@@ -87,6 +87,19 @@ function! go#lsp#message#DidClose(file)
        \ }
 endfunction
 
+function! go#lsp#message#Completion(file, line, col)
+  return {
+          \ 'notification': 0,
+          \ 'method': 'textDocument/completion',
+          \ 'params': {
+          \   'textDocument': {
+          \       'uri': go#path#ToURI(a:file)
+          \   },
+          \   'position': s:position(a:line, a:col),
+          \ }
+       \ }
+endfunction
+
 function! s:position(line, col)
   return {'line': a:line - 1, 'character': a:col-1}
 endfunction

ftplugin/go.vim
@@ -25,8 +25,11 @@ setlocal noexpandtab
 
 compiler go
 
-" Set gocode completion
+" Set autocompletion
 setlocal omnifunc=go#complete#Complete
+if !go#util#has_job()
+  setlocal omnifunc=go#complete#GocodeComplete
+endif
 
 if get(g:, "go_doc_keywordprg_enabled", 1)
   " keywordprg doesn't allow to use vim commands, override it

commit 9495669281e56570527400ad540990b86ca47bdb

completion: fix gocode processing
Fix the processing of matches from gocode. This was accidentally broken
in dac81d653dfcec98624a016dc3ba854959d7df9b when the processing of the
gocode results was changed so that the response array was trimmed to the
dictionary in the initial processing of the results.

autoload/go/complete.vim
@@ -242,8 +242,7 @@ function! go#complete#GocodeComplete(findstart, base) abort
     if s =~ '[(){}\{\}]'
       return map(copy(s:completions[1]), 's:trim_bracket(v:val)')
     endif
-
-    return s:completions[1]
+    return s:completions
   endif
 endfunction
 